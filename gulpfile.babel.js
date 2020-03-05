cf04db003406f8a0c7179407f5612c539e2803a7
Hiep Nguyen <thienhiep171193@gmail.com>
2020-03-06 00:05:07 +0700

    Update gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,84 +1,83 @@
-import { src, dest, watch, parallel, series } from 'gulp';
-import del from 'del';
-import babel from 'gulp-babel';
-import merge from 'merge-stream';
-import sourcemaps from 'gulp-sourcemaps';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
-import cleanCSS from 'gulp-clean-css';
-import imagemin from 'gulp-imagemin';
-import flatten from 'gulp-flatten';
-import rename from 'gulp-rename';
-import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
-import pug from 'gulp-pug';
-const browsersync = require('browser-sync').create();
+import { src, dest, watch, parallel, series, task } from "gulp";
+import del from "del";
+import babel from "gulp-babel";
+import sourcemaps from "gulp-sourcemaps";
+import plumber from "gulp-plumber";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin from "gulp-imagemin";
+import flatten from "gulp-flatten";
+import rename from "gulp-rename";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import pug from "gulp-pug";
+const browsersync = require("browser-sync").create();
 
 const files = {
   global: {
-    dist: 'dist',
-    src: 'src'
+    dist: "dist",
+    src: "src"
   },
   bootstrap: {
-    src: 'src/scss/vendors/bootstrap',
-    glob: 'src/scss/vendors/bootstrap/bootstrap.scss',
-    dest: 'dist/css'
+    src: "src/scss/vendors/bootstrap",
+    glob: "src/scss/vendors/bootstrap/bootstrap.scss",
+    dest: "dist/css"
   },
   css: {
-    src: 'src/scss',
-    glob: 'src/scss/**/!(plugins).scss',
-    dest: 'dist/css',
-    excl: '!src/scss/vendors/**/*.scss'
+    src: "src/scss",
+    glob: "src/scss/**/!(plugins|custons).scss",
+    dest: "dist/css",
+    excl: "!src/scss/vendors/**/*.scss"
   },
   cssPlugins: {
-    src: 'src/scss',
-    glob: 'src/scss/plugins.scss',
-    dest: 'dist/css'
+    src: "src/scss",
+    glob: "src/scss/plugins.scss",
+    dest: "dist/css"
   },
   fonts: {
-    src: 'src/fonts',
-    glob: 'src/fonts/**/*',
-    dest: 'dist/fonts'
+    src: "src/fonts",
+    glob: "src/fonts/**/*",
+    dest: "dist/fonts"
   },
   images: {
-    src: 'src/img',
-    glob: 'src/img/**/*',
-    dest: 'dist/img'
+    src: "src/img",
+    glob: "src/img/**/*",
+    dest: "dist/img"
   },
   scriptCustoms: {
-    src: 'src/js',
-    glob: 'src/js/include/custom/*.js',
-    dest: 'dist/js'
+    src: "src/js",
+    glob: "src/js/include/custom/*.js",
+    dest: "dist/js"
   },
   scriptVendors: {
-    src: 'src/js',
-    glob: 'src/js/include/vendor/*.js',
-    dest: 'dist/js'
+    src: "src/js",
+    glob: "src/js/include/vendor/*.js",
+    dest: "dist/js"
   },
   scriptLibs: {
-    src: 'src/js',
-    glob: 'src/js/include/plugin/*.js',
-    dest: 'dist/js'
+    src: "src/js",
+    glob: "src/js/include/plugin/*.js",
+    dest: "dist/js"
   },
   scriptPlugins: {
-    src: 'src/js',
-    glob: 'src/js/include/plugins/*.js',
-    dest: 'dist/js'
+    src: "src/js",
+    glob: "src/js/include/plugins/*.js",
+    dest: "dist/js"
   },
   templates: {
-    src: 'src/pug',
-    glob: 'src/pug/**/*.pug',
-    dest: 'dist'
+    src: "src/pug",
+    glob: "src/pug/pages/*.pug",
+    watcher: "src/pug/**/*.pug",
+    dest: "dist"
   },
   maps: {
-    css: 'dist/maps/css',
-    script: 'dist/maps/script',
-    src: 'dist/maps'
+    css: "../maps/css",
+    script: "../maps/script",
+    src: "dist/maps"
   }
 };
 
-export const browserSync = (done) => {
+export const browserSync = done => {
   browsersync.init({
     server: {
       baseDir: files.global.dist
@@ -86,22 +85,22 @@ export const browserSync = (done) => {
     port: 3000
   });
   done();
-}
+};
 
-export const browserSyncReload = (done) => {
+export const browserSyncReload = done => {
   browsersync.reload();
   done();
-}
+};
 
 // Styles tasks
-export const styleBootstrap = () => {
+export const styleBootstrap = done => {
   src(files.bootstrap.glob)
     .pipe(sourcemaps.init())
     .pipe(
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
@@ -111,27 +110,22 @@ export const styleBootstrap = () => {
         loadMaps: true
       })
     )
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(autoprefixer("last 2 versions"))
     .pipe(dest(files.bootstrap.dest))
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(cleanCSS())
     .pipe(sourcemaps.write(files.maps.css))
     .pipe(dest(files.bootstrap.dest))
     .pipe(browsersync.stream());
+  done();
 };
 
-export const styleMain = () => {
+export const styleMain = done => {
   src([files.css.glob, files.css.excl])
     .pipe(sourcemaps.init())
     .pipe(
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
@@ -141,27 +135,22 @@ export const styleMain = () => {
         loadMaps: true
       })
     )
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(autoprefixer("last 2 versions"))
     .pipe(dest(files.css.dest))
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(cleanCSS())
     .pipe(sourcemaps.write(files.maps.css))
     .pipe(dest(files.css.dest))
     .pipe(browsersync.stream());
+  done();
 };
 
-export const stylePlugin = () => {
+export const stylePlugin = done => {
   src([files.cssPlugins.glob])
     .pipe(sourcemaps.init())
     .pipe(
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
@@ -171,43 +160,38 @@ export const stylePlugin = () => {
         loadMaps: true
       })
     )
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(autoprefixer("last 2 versions"))
     .pipe(dest(files.cssPlugins.dest))
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(cleanCSS())
     .pipe(sourcemaps.write(files.maps.css))
     .pipe(dest(files.cssPlugins.dest))
     .pipe(browsersync.stream());
+  done();
 };
 
 // Javascript tasks
-export const scriptVendors = () => {
+export const scriptVendors = done => {
   src(files.scriptVendors.glob)
     .pipe(
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
-    .pipe(concat('vendors.js'))
+    .pipe(concat("vendors.js"))
     .pipe(dest(files.scriptVendors.dest))
     .pipe(
       rename({
-        suffix: '.min'
+        suffix: ".min"
       })
     )
     .pipe(uglify())
-    .pipe(dest(files.scriptVendors.dest))
-    .pipe(browsersync.stream());
+    .pipe(dest(files.scriptVendors.dest));
+  done();
 };
 
-export const scriptLibs = () => {
+export const scriptLibs = done => {
   src(files.scriptLibs.glob)
     .pipe(
       sourcemaps.init({
@@ -218,32 +202,32 @@ export const scriptLibs = () => {
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
-    .pipe(concat('plugins.js'))
+    .pipe(concat("plugins.js"))
     .pipe(dest(files.scriptLibs.dest))
     .pipe(sourcemaps.write(files.maps.script))
-    .pipe(dest(files.scriptLibs.dest))
-    .pipe(browsersync.stream());
+    .pipe(dest(files.scriptLibs.dest));
+  done();
 };
 
-export const scriptPlugins = () => {
+export const scriptPlugins = done => {
   src(files.scriptPlugins.glob)
     .pipe(
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
-    .pipe(dest(files.scriptPlugins.dest))
-    .pipe(browsersync.stream());
+    .pipe(dest(files.scriptPlugins.dest));
+  done();
 };
 
-export const scriptCustoms = () => {
+export const scriptCustoms = done => {
   src(files.scriptCustoms.glob)
     .pipe(
       sourcemaps.init({
@@ -254,20 +238,20 @@ export const scriptCustoms = () => {
       plumber({
         errorHandler: function(error) {
           console.log(error.message);
-          this.emit('end');
+          this.emit("end");
         }
       })
     )
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(concat('customs.js'))
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(concat("customs.js"))
     .pipe(dest(files.scriptCustoms.dest))
     .pipe(sourcemaps.write(files.maps.script))
-    .pipe(dest(files.scriptCustoms.dest))
-    .pipe(browsersync.stream());
+    .pipe(dest(files.scriptCustoms.dest));
+  done();
 };
 
 // Html task
-export const templates = () => {
+export const templates = done => {
   src(files.templates.glob)
     .pipe(
       pug({
@@ -275,20 +259,31 @@ export const templates = () => {
         cache: true
       })
     )
-    .on('error', function(err) {
-      process.stderr.write(err.message + '\n');
-      this.emit('end');
+    .on("error", function(err) {
+      process.stderr.write(err.message + "\n");
+      this.emit("end");
     })
     .pipe(dest(files.templates.dest));
+  done();
 };
 
 // Fonts task
-export const fonts = () => {
-  src(files.fonts.glob).pipe(files.fonts.dest);
+export const fonts = done => {
+  src(files.fonts.glob)
+    .pipe(
+      plumber({
+        errorHandler: function(error) {
+          console.log(error.message);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(dest(files.fonts.dest));
+  done();
 };
 
 // Images task
-export const images = () => {
+export const images = done => {
   src(files.images.glob)
     .pipe(imagemin())
     .pipe(
@@ -297,16 +292,18 @@ export const images = () => {
       })
     )
     .pipe(dest(files.images.dest));
+  done();
 };
 
 // Clean sourcemaps task
-export const cleanSourceMap = () => {
+export const cleanSourceMap = done => {
   del(files.maps.src, {
     force: true
   });
+  done();
 };
 
-export const watchAll = () => {
+export const dev = done => {
   browsersync.init({
     server: {
       baseDir: files.global.dist
@@ -318,11 +315,29 @@ export const watchAll = () => {
   watch(files.bootstrap.glob, series(styleBootstrap));
   watch(files.css.glob, series(styleMain));
   watch(files.cssPlugins.glob, series(stylePlugin));
-  watch(files.scriptCustoms.glob, series(scriptCustoms));
-  watch(files.scriptLibs.glob, series(scriptLibs));
-  watch(files.scriptVendors.glob, series(scriptVendors));
-  watch(files.scriptPlugins.glob, series(scriptPlugins));
-  watch(files.templates.glob, series(templates));
-  watch(files.fonts.glob, series(fonts));
-  watch(files.images.glob, series(images));
+  watch(files.scriptCustoms.glob, series(scriptCustoms, browserSyncReload));
+  watch(files.scriptLibs.glob, series(scriptLibs, browserSyncReload));
+  watch(files.scriptVendors.glob, series(scriptVendors, browserSyncReload));
+  watch(files.scriptPlugins.glob, series(scriptPlugins, browserSyncReload));
+  watch(files.templates.watcher, series(templates, browserSyncReload));
+  watch(files.fonts.glob, series(fonts, browserSyncReload));
+  watch(files.images.glob, series(images, browserSyncReload));
+  done();
 };
+
+export const build = parallel(
+  styleBootstrap,
+  styleMain,
+  stylePlugin,
+  scriptCustoms,
+  scriptLibs,
+  scriptVendors,
+  scriptPlugins,
+  templates,
+  images,
+  fonts
+);
+
+export const clean = series(
+  cleanSourceMap
+);
