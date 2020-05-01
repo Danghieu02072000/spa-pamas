21513d3c2aa8fd38124884ac1bc7ae7a834aea97
Hiep Nguyen <thienhiep171193@gmail.com>
2020-05-01 18:06:48 +0700

    Refactor sidebar module to Vanilla JS

--- src/js/include/custom/sidebar.js
+++ src/js/include/custom/sidebar.js
@@ -1,30 +1,65 @@
-/* global $ */
 import { disableBodyScroll, clearAllBodyScrollLocks } from '../lib/bodyScrollLock'
+import { select, on, addClass, hasClass, removeClass } from '../lib/dom'
+
+const BODY = document.body
+const SIDEBAR_SELECTOR = '.js-sidebar'
+const SIDEBAR_OVERLAY_SELECTOR = '.js-sidebar-overlay'
+const SLIDEOUT_SELECTOR = '.js-slideout'
+const SLIDEOUT_OVERLAY_SELECTOR = '.js-slideout-overlay'
+const SLIDEOUT_TOGGLE_SELECTOR = '.js-slideout-toggle'
+const SLIDEOUT_ACTIVE_CLASS = 'is-slideout-active'
+const SEARCH_TRIGGER_SELECTOR = '.js-search-trigger'
+const SEARCH_ACTIVE_CLASS = 'is-search-active'
 
 export default () => {
-  const slideoutOverlay = $('.js-slideout-overlay')
-  const sidebarOverlay = $('.js-sidebar__overlay')
-  const sidebar = $('#sidebar')
-  const iconSidebar = $('#sidebar-icon')
-  const wrapper = $('#wrapper')
+  const el = select(SIDEBAR_SELECTOR)
+  if (el) {
+    const slideoutEl = select(SLIDEOUT_SELECTOR, el)
+    const slideoutToggleEl = select(SLIDEOUT_TOGGLE_SELECTOR, el)
+    const slideoutOverlayEl = select(SLIDEOUT_OVERLAY_SELECTOR, el)
+    const sidebarOverlayEl = select(SIDEBAR_OVERLAY_SELECTOR, el)
+    const searchTriggerEl = select(SEARCH_TRIGGER_SELECTOR, el)
+    if (slideoutToggleEl && slideoutEl) {
+      on('click', () => {
+        if (!hasClass(SLIDEOUT_ACTIVE_CLASS, BODY)) {
+          addClass(SLIDEOUT_ACTIVE_CLASS, BODY)
+          disableBodyScroll(slideoutEl)
+        } else {
+          removeClass(SLIDEOUT_ACTIVE_CLASS, BODY)
+          clearAllBodyScrollLocks()
+        }
+      }, slideoutToggleEl)
+    }
 
-  $('.js-slideout-toggle').on('click', function () {
-    wrapper.toggleClass('is-slideout-active')
-    disableBodyScroll()
-  })
-  slideoutOverlay.on('click', function () {
-    sidebar.removeClass('open')
-    wrapper.removeClass('is-slideout-active')
-    iconSidebar.removeClass('active')
-    clearAllBodyScrollLocks()
-  })
+    if (slideoutOverlayEl) {
+      on('click', () => {
+        if (!hasClass(SLIDEOUT_ACTIVE_CLASS, BODY)) {
+          addClass(SLIDEOUT_ACTIVE_CLASS, BODY)
+          disableBodyScroll(slideoutEl)
+        } else {
+          removeClass(SLIDEOUT_ACTIVE_CLASS, BODY)
+          clearAllBodyScrollLocks()
+        }
+      }, slideoutOverlayEl)
+    }
 
-  sidebarOverlay.on('click', function () {
-    wrapper.removeClass('is-search-active')
-  })
+    if (searchTriggerEl) {
+      on('click', () => {
+        if (!hasClass(SEARCH_ACTIVE_CLASS, BODY)) {
+          addClass(SEARCH_ACTIVE_CLASS, BODY)
+          disableBodyScroll(BODY)
+        } else {
+          removeClass(SEARCH_ACTIVE_CLASS, BODY)
+          clearAllBodyScrollLocks()
+        }
+      }, searchTriggerEl)
+    }
 
-  $('.js-search-trigger').on('click', function () {
-    wrapper.addClass('is-search-active')
-    $('#iptSearchMobie').focus()
-  })
+    if (sidebarOverlayEl) {
+      on('click', () => {
+        removeClass(SEARCH_ACTIVE_CLASS, BODY)
+        clearAllBodyScrollLocks()
+      }, sidebarOverlayEl)
+    }
+  }
 }
